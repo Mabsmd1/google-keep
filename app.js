af836543f705cb4d9366d5780b0e6fbc677621c9
Mabel Sinah <moagams10@gmail.com>
2023-11-11 22:19:17 +0200

    Implement authentication using Goole and Ngrok

--- app.js
+++ app.js
@@ -30,18 +30,57 @@ class App {
     this.$sidebar = document.querySelector(".sidebar");
     this.$sidebarActiveItem = document.querySelector(".active-item");
 
-    // Initialize the FirebaseUI Widget using Firebase.
-    this.ui = new firebaseui.auth.AuthUI(auth);
+    this.$app = document.querySelector("#app");
+    this.$firebaseAuthContainer = document.querySelector("#firebaseui-auth-container");
+    this.$authUserText = document.querySelector(".auth-user");
+    this.$logoutButton = document.querySelector(".logout");
 
-    this.ui.start('#firebaseui-auth-container', {
-      signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
-      ],
-      // Other config options...
-    });
+    
+    this.ui = new firebaseui.auth.AuthUI(auth);
+    this.handleAuth();
 
     this.addEventListeners();
     this.displayNotes();
+  }
+
+  handleAuth() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log(user);
+        this.$authUserText.innerHTML = user.displayName;
+        this.redirectToApp();
+      } else {
+        this.redirectToAuth();
+      }
+    });
+    
+  }
+
+  handleLogout() {
+    firebase.auth().signOut().then(() => {
+      this.redirectToAuth();
+    }).catch((error) => {
+      console.log("ERROR OCCURED", error);
+    });
+  }
+
+  redirectToApp() {
+    this.$firebaseAuthContainer.style.display = "none";
+    this.$app.style.display = "block";
+  }
+
+  redirectToAuth() {
+    this.$firebaseAuthContainer.style.display = "block";
+    this.$app.style.display = "none";
+
+    this.ui.start('#firebaseui-auth-container', {
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        
+      ],
+      // Other config options...
+    });
   }
 
   addEventListeners() {
@@ -70,6 +109,10 @@ class App {
 
     this.$sidebar.addEventListener("mouseout", (event) => {
       this.handleToggleSidebar();
+    })
+
+    this.$logoutButton.addEventListener("click", (event) => {
+      this.handleLogout();
     })
 
   }
